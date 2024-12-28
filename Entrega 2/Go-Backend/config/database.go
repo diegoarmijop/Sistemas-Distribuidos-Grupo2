3e5dfac8b058821fee846b6f815a6bbe50a138d0
Matias Yanez <matias.yanez@usach.cl>
2024-12-28 10:52:56 -0300

    fix en las migraciones

--- Entrega 2/Go-Backend/config/database.go
+++ Entrega 2/Go-Backend/config/database.go
@@ -13,12 +13,17 @@ import (
 
 var DB *gorm.DB
 
-func ConnectDB() {
+// loadEnvVariables carga las variables de entorno desde el archivo .env
+func loadEnvVariables() error {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Error cargando archivo .env")
+		return fmt.Errorf("error cargando archivo .env: %v", err)
 	}
+	return nil
+}
 
+// getDBConfig obtiene las configuraciones de la base de datos desde las variables de entorno
+func getDBConfig() (string, error) {
 	dbHost := os.Getenv("DB_HOST")
 	dbUser := os.Getenv("DB_USER")
 	dbPassword := os.Getenv("DB_PASSWORD")
@@ -27,19 +32,76 @@ func ConnectDB() {
 	dbSslmode := os.Getenv("DB_SSLMODE")
 	dbTimezone := os.Getenv("DB_TIMEZONE")
 
+	// Verificamos si alguna variable de entorno esencial está vacía
+	if dbHost == "" || dbUser == "" || dbPassword == "" || dbName == "" || dbPort == "" || dbSslmode == "" || dbTimezone == "" {
+		return "", fmt.Errorf("faltan algunas variables de entorno necesarias para la conexión")
+	}
+
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=%s",
 		dbHost, dbUser, dbPassword, dbName, dbPort, dbSslmode, dbTimezone)
 
+	return dsn, nil
+}
+
+// connectToDatabase conecta a la base de datos utilizando GORM
+func connectToDatabase(dsn string) error {
+	var err error
 	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Error al conectar a la base de datos: ", err)
+		return fmt.Errorf("error al conectar a la base de datos: %v", err)
+	}
+	return nil
+}
+
+// migrateDatabase realiza las migraciones automáticas en la base de datos
+func migrateDatabase() error {
+	// Primero migramos las tablas sin claves foráneas
+	err := DB.AutoMigrate(
+		&models.PestType{}, // Debe migrarse primero
+	)
+	if err != nil {
+		return fmt.Errorf("error en la auto-migración de PestType: %v", err)
+	}
+
+	// Luego migramos las tablas que dependen de claves foráneas
+	err = DB.AutoMigrate(
+		&models.PlagueEvent{},
+		&models.Alert{}, // Relacionado con PlagueEvent
+		&models.Camp{},
+		&models.Configuration{},
+		&models.Sensor{},
+		&models.User{},
+	)
+	if err != nil {
+		return fmt.Errorf("error en la auto-migración de PlagueEvent y otras tablas: %v", err)
+	}
+
+	return nil
+}
+
+// ConnectDB es la función principal que conecta a la base de datos y realiza la migración
+func ConnectDB() {
+	// Cargar variables de entorno
+	if err := loadEnvVariables(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Obtener configuración de la base de datos
+	dsn, err := getDBConfig()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Conectar a la base de datos
+	if err := connectToDatabase(dsn); err != nil {
+		log.Fatal(err)
 	}
 
 	log.Println("Conexión a la base de datos exitosa")
 
-	err = DB.AutoMigrate(&models.User{})
-	if err != nil {
-		log.Fatal("Error en la auto-migración:", err)
+	// Realizar migraciones
+	if err := migrateDatabase(); err != nil {
+		log.Fatal(err)
 	}
 
 	log.Println("Migración de base de datos completada")
